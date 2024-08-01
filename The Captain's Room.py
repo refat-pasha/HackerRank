1b88441171e9b1f09bafddf41a57287d99052ee4
turtle <refatpasha567@gmail.com>
2024-08-02 02:31:56 +0600

    Update The Captain's Room.py

--- The Captain's Room.py
+++ The Captain's Room.py
@@ -1,9 +1,10 @@
-a = int(input())
-original_list = list(map(int, input().split()))
-unique_list = []
-for item in original_list:
-    if item != original_list:
-        x = unique_list.append(item)
-print(x)
+def coptain_room(rooms, k):
+	myset = set(rooms)
+	ans = (sum(myset)*k) - sum(rooms)
+	return (ans // (k-1))
 
 
+if __name__ == '__main__':
+	k = int( input() )
+	rooms = list(map(int, input().split()))
+	print(coptain_room(rooms, k))
